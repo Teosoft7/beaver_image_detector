c8bf2a4314cb133bc9401886e6dc3ee748c95ce4
Natasha Kacoroski <nkacoroski@gmail.com>
2019-07-10 15:35:47 -0700

    Complete initial cleaning of notebooks

--- ddb/model_functions/detection_functions.py
+++ ddb/model_functions/detection_functions.py
@@ -98,6 +98,32 @@ def run_inference_for_single_image(image, graph):
     return output_dict
 
 
+def predict_image(image_path):
+    """Take in image and return image with predictions."""
+    image = Image.open(image_path)
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
+    image_np = load_image_into_numpy_array(image)
+  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+  # Actual detection.
+    output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+    name = category_index[output_dict['detection_classes'][0]]['name']
+    confidence = np.round(output_dict['detection_scores'][0], 2)
+  # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=10)
+    plt.figure(figsize=(8,12))
+    plt.imshow()
+
+    
 def display_prediction(image_path):
     """Take in image and return image with predictions."""
     image = Image.open(image_path)
